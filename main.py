5a5f78a20fbbd40a16cea2b6fd2bddf4d8677fdd
camiloa2m <camiloaww@gmail.com>
2024-09-28 15:11:51 -0500

    implemented basic auth

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import base64
 import pickle
+import secrets
 from io import BytesIO
 
 import numpy as np
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.encoders import jsonable_encoder
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from PIL import Image
 from pydantic import BaseModel
 
@@ -13,6 +15,7 @@ model_path = "model/clf.pickle"
 model = pickle.load(open(model_path, "rb"))
 
 app = FastAPI()
+security = HTTPBasic()  # Utilizamos seguridad básica
 
 
 class SubjectModel(BaseModel):
@@ -43,17 +46,48 @@ def preprocess_image(image: Image.Image) -> np.ndarray:
     return image
 
 
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    # Definimos el nombre de usuario y la contraseña correctos
+    correct_username = "admin"
+    correct_password = "password123"
+
+    correct_username_bytes = bytes(correct_username, "utf8")
+    correct_password_bytes = bytes(correct_password, "utf8")
+
+    user_current_username_bytes = credentials.username.encode("utf8")
+    user_current_password_bytes = credentials.password.encode("utf8")
+
+    # Comparamos las credenciales proporcionadas
+    is_correct_username = secrets.compare_digest(
+        user_current_username_bytes, correct_username_bytes
+    )
+    is_correct_password = secrets.compare_digest(
+        user_current_password_bytes, correct_password_bytes
+    )
+
+    # Verificamos que el nombre de usuario y la contraseña coincidan
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+
 @app.get("/")
 def read_root():
     return {"msg": "Digit Image Classification."}
 
 
 @app.post("/predict")
-def predict_class(input: Input):
+def predict_class(
+    input: Input, credentials: HTTPBasicCredentials = Depends(authenticate)
+):
     data = jsonable_encoder(input)
 
     if data["subject_type"] != "Image":
         raise HTTPException(status_code=600, detail="Subject_type is not 'Image'")
+
     if data["subject"]["type"] == "base64":
         img_b64 = data["subject"]["value"]
     else:
